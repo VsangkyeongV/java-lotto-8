460d94a31a9fd8351a68cbda767922a92d3c5830
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-11-03 00:02:25 +0900

    refactor: 구조 변경

--- src/main/java/lotto/valid/PayValid.java
+++ src/main/java/lotto/valid/PayValid.java
@@ -2,41 +2,43 @@ package lotto.valid;
 
 public class PayValid {
 
-    int amount;
+    private String pay;
+    private int amount;
     final String ERROR_PAY_EMPTY_MESSAGE = "[ERROR] 금액을 입력해 주세요.";
     final String ERROR_PAY_NUMBER_MESSAGE = "[ERROR] 금액은 숫자여야 합니다.";
     final String ERROR_PAY_POSITIVE_MESSAGE = "[ERROR] 금액은 양수여야 합니다.";
     final String ERROR_PAY_THOUSAND_MESSAGE = "[ERROR] 금액은 1000원 단위여야 합니다.";
 
     public PayValid(String pay) {
-        isEmpty(pay);
-        isNum(pay);
-        isPositive(amount);
-        isThousand(amount);
+        this.pay = pay;
+        isEmpty();
+        isNum();
+        isPositive();
+        isThousand();
     }
 
-    void isEmpty(String str) {
-        if (str == null || str.isEmpty()) {
+    void isEmpty() {
+        if (pay == null || pay.isEmpty()) {
             throw new IllegalArgumentException(ERROR_PAY_EMPTY_MESSAGE);
         }
     }
 
-    void isNum(String str) {
+    void isNum() {
         try {
-            amount = Integer.parseInt(str);
+            amount = Integer.parseInt(pay);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_PAY_NUMBER_MESSAGE);
         }
     }
 
-    void isPositive(int num) {
-        if (num <= 0) {
+    void isPositive() {
+        if (amount <= 0) {
             throw new IllegalArgumentException(ERROR_PAY_POSITIVE_MESSAGE);
         }
     }
 
-    void isThousand(int num) {
-        if (num < 1000 || num % 1000 != 0) {
+    void isThousand() {
+        if (amount < 1000 || amount % 1000 != 0) {
             throw new IllegalArgumentException(ERROR_PAY_THOUSAND_MESSAGE);
         }
     }
