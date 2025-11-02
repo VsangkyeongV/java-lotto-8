875556bb5a47354811eda2464ce2bd8446416ff5
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-11-02 23:57:24 +0900

    refactor: 구조 수정

--- src/main/java/lotto/valid/PayValid.java
+++ src/main/java/lotto/valid/PayValid.java
@@ -3,12 +3,12 @@ package lotto.valid;
 public class PayValid {
 
     int amount;
-    final String ERROR_EMPTY_MESSAGE = "[ERROR] 금액을 입력해 주세요.";
-    final String ERROR_NUMBER_MESSAGE = "[ERROR] 금액은 숫자여야 합니다.";
-    final String ERROR_POSITIVE_MESSAGE = "[ERROR] 금액은 양수여야 합니다.";
-    final String ERROR_THOUSAND_MESSAGE = "[ERROR] 금액은 1000원 단위여야 합니다.";
+    final String ERROR_PAY_EMPTY_MESSAGE = "[ERROR] 금액을 입력해 주세요.";
+    final String ERROR_PAY_NUMBER_MESSAGE = "[ERROR] 금액은 숫자여야 합니다.";
+    final String ERROR_PAY_POSITIVE_MESSAGE = "[ERROR] 금액은 양수여야 합니다.";
+    final String ERROR_PAY_THOUSAND_MESSAGE = "[ERROR] 금액은 1000원 단위여야 합니다.";
 
-    public void run(String pay) {
+    public PayValid(String pay) {
         isEmpty(pay);
         isNum(pay);
         isPositive(amount);
@@ -17,7 +17,7 @@ public class PayValid {
 
     void isEmpty(String str) {
         if (str == null || str.isEmpty()) {
-            throw new IllegalArgumentException(ERROR_EMPTY_MESSAGE);
+            throw new IllegalArgumentException(ERROR_PAY_EMPTY_MESSAGE);
         }
     }
 
@@ -25,19 +25,19 @@ public class PayValid {
         try {
             amount = Integer.parseInt(str);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ERROR_NUMBER_MESSAGE);
+            throw new IllegalArgumentException(ERROR_PAY_NUMBER_MESSAGE);
         }
     }
 
     void isPositive(int num) {
         if (num <= 0) {
-            throw new IllegalArgumentException(ERROR_POSITIVE_MESSAGE);
+            throw new IllegalArgumentException(ERROR_PAY_POSITIVE_MESSAGE);
         }
     }
 
     void isThousand(int num) {
         if (num < 1000 || num % 1000 != 0) {
-            throw new IllegalArgumentException(ERROR_THOUSAND_MESSAGE);
+            throw new IllegalArgumentException(ERROR_PAY_THOUSAND_MESSAGE);
         }
     }
 
