97dcf4d0d00e9a7626959a548f0c679925fcedfd
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-11-03 00:46:02 +0900

    feat: 당첨 번호 유효성 test코드 구현

---
+++ src/test/java/lotto/WinningNumbersValidTest.java
@@ -0,0 +1,39 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.Test;
+
+import lotto.valid.WinningNumbersValid;
+
+class WinningNumbersValidTest {
+
+    @Test
+    void 당첨_번호_입력_안하면_예외_발생() {
+        assertThatThrownBy(() -> new WinningNumbersValid(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호를 입력해 주세요");
+    }
+
+    @Test
+    void 당첨_번호_쉼표_구분자_아니면_예외_발생() {
+        assertThatThrownBy(() -> new WinningNumbersValid(
+                "1.2;3/4'5[6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호는 쉼표(,)로 구분해 주세요");
+    }
+
+    @Test
+    void 당첨_번호_숫자_아니면_예외_발생() {
+        assertThatThrownBy(() -> new WinningNumbersValid("1,2,3,*,5,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호는 숫자여야 합니다");
+    }
+
+    @Test
+    void 당첨_번호_범위_벗어나면_예외_발생() {
+        assertThatThrownBy(() -> new WinningNumbersValid("-1,2,3,4,5,46"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("당첨 번호는 1부터 45 사이의 숫자여야 합니다");
+    }
+
+}
