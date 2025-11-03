f4f048cfa35b62a498a2395991a2f6a645015698
sangkyeongJeong <137258368+VsangkyeongV@users.noreply.github.com>
2025-11-03 23:06:17 +0900

    feat: 메인app구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,64 @@
 package lotto;
 
+import java.util.List;
+
+import lotto.valid.BonusNumberValid;
+import lotto.valid.PayValid;
+import lotto.valid.WinningNumbersValid;
+import lotto.view.InputView;
+
 public class Application {
+
+    private final InputView inputView = new InputView();
+
+    void run() {
+        String pay;
+        while (true) {
+            try {
+                pay = inputView.readPay();
+                new PayValid(pay);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        LottoMachine lottoMachine = new LottoMachine(pay);
+        List<Lotto> lottos = lottoMachine.makeLottos();
+        lottoMachine.printResult();
+
+        String winningNumbers;
+        while (true) {
+            try {
+                winningNumbers = inputView.readWinningNumbers();
+                new WinningNumbersValid(winningNumbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        String bonusNumber;
+        while (true) {
+            try {
+                bonusNumber = inputView.readBonusNumber();
+                new BonusNumberValid(bonusNumber);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        WinResult winResult = new WinResult(winningNumbers, bonusNumber);
+        winResult.parseWinningNumbers();
+        winResult.countMatches(lottos);
+
+        System.out.println("\n당첨 통계\n---\n");
+        System.out.print(winResult.printWins());
+        System.out.println(winResult.printProfitRate(Integer.parseInt(pay)));
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Application().run();
     }
 }
